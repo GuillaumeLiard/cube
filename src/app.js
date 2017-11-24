01e248266abe0143b258def64b1ece0e225f0198
GuillaumeLiard <liardg@gmail.com>
2017-11-24 19:48:48 +0100

    added orbit controls

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import * as THREE from 'three';
+const OrbitControls = require('three-orbit-controls')(THREE);
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
+var controls = new OrbitControls(camera)
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var material = new THREE.ShaderMaterial({
-    vertexShader: document.getElementById('vertexShader').textContent,
-    fragmentShader: document.getElementById('fragmentShader').textContent
-});
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// var material = new THREE.ShaderMaterial({
+//     vertexShader: document.getElementById('vertexShader').textContent,
+//     fragmentShader: document.getElementById('fragmentShader').textContent
+// });
 
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
@@ -22,8 +23,8 @@ camera.position.z = 5;
 var animate = function () {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 };
