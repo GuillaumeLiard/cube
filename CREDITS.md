1a0a7e9fb96f052828b06ec750a970f416ac990d
GuillaumeLiard <liardg@gmail.com>
2017-11-27 20:39:03 +0100

    cleanup + added credits

--- CREDITS.md
+++ CREDITS.md
@@ -1,3 +1,3 @@
 https://www.youtube.com/watch?v=uD4GnMsAH1U
 https://www.sitepoint.com/how-to-build-a-todo-app-using-react-redux-and-immutable-js/
-// http://glslsandbox.com/e#43792.1
+http://glslsandbox.com/e#43792.1
--- src/app.js
+++ src/app.js
@@ -8,22 +8,7 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// var geometry = new THREE.BufferGeometry();
-// var vertices = new Float32Array( [
-// 	-1.0, -1.0,  1.0,
-// 	 1.0, -1.0,  1.0,
-// 	 1.0,  1.0,  1.0,
-//
-// 	 1.0,  1.0,  1.0,
-// 	-1.0,  1.0,  1.0,
-// 	-1.0, -1.0,  1.0
-// ] );
-//
-// // itemSize = 3 because there are 3 values (components) per vertex
-// geometry.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
 var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
-// var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var material = new THREE.ShaderMaterial({
     uniforms: { "time": { value: 0.5 }, "time2": { value: 2 } },
     vertexShader: document.getElementById('vertexShader').textContent,
@@ -34,14 +19,10 @@ var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
-console.log(cube.material);
+// console.log(cube.material);
 
 var animate = function () {
     requestAnimationFrame( animate );
-
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    // uniforms.time.value += 0.01;
     cube.material.uniforms.time.value += 0.1;
     cube.material.uniforms.time2.value += 0.1;
     renderer.render(scene, camera);
